0a6b9d38acc1975c66f2abab24c30e0a0aa35598
Nelson Pereira Pinto Neto <nelson.pereira@neadunimes.net>
2018-09-20 12:02:25 -0300

    Inicio de Projeto

---
+++ templates/template.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/arquivo-morto.css">
+    <title>Arquivo Morto</title>
+</head>
+<body>
+<h1>Arquivo Morto</h1>
+
+
+
+<script src="../js/bootstrap.min.js"></script>
+</body>
+</html>
